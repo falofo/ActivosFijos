51cc6076951c38fa2def22b824b701c4cdd9ae42
falofo <falofo23@gmail.com>
2018-11-10 12:18:33 -0500

    Se agrega estructura del proyecto y dependencias en el pom

---
+++ src/main/java/com/ads/activosfijos/ActivosApplication.java
@@ -0,0 +1,14 @@
+package com.ads.activosfijos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Fabiani Lozano on 10/11/2018.
+ */
+@SpringBootApplication
+public class ActivosApplication {
+    public  static void main(String[] args){
+        SpringApplication.run(ActivosApplication.class,args);
+    }
+}
